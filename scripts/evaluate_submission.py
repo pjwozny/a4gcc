FIX wrong axis for mean calculation

scripts/evaluate_submission.py
@@ -301,7 +301,7 @@ def compute_metrics(fetch_episode_states, trainer, framework, submission_file, e
 
                 if feature.endswith("_all_regions"):
                     title = f"mean_{feature.rsplit('_', 2)[0]}_over_regions"
-                    ys_mean = np.mean(ys, axis=1)
+                    ys_mean = np.mean(ys, axis=0)
                     data = [[x, y] for (x, y) in zip(xs, ys_mean.tolist())]
                     table = wandb.Table(data=data, columns = ["Steps", "y"])
                     wandb.log({title : wandb.plot.line(table, "Steps", "y",