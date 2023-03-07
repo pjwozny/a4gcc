b43a6844798bfa34131d1cf501c3ce368d390115
Bram Renting <bramrenting@gmail.com>
2023-03-07 11:58:36 +0100

    FIX covert time to string

--- scripts/evaluate_submission.py
+++ scripts/evaluate_submission.py
@@ -562,7 +562,7 @@ if __name__ == "__main__":
     try:
         # Also handle a zipped file
         if results_dir.endswith(".zip"):
-            unzipped_results_dir = Path(tempfile.gettempdir()) / time.time()
+            unzipped_results_dir = Path(tempfile.gettempdir()) / str(time.time())
             shutil.unpack_archive(results_dir, unzipped_results_dir)
             results_dir = unzipped_results_dir
     except Exception as err:
